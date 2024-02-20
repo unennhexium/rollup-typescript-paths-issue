7f0aa60efda9264f9843e914ded62b2ea170e1a4
unennhexium <50716292+unennhexium@users.noreply.github>
2024-02-20 19:45:44 +0300

    Remove defineConfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,16 @@
-import { defineConfig } from 'rollup'
 import { typescriptPaths } from 'rollup-plugin-typescript-paths'
 import esbuild from 'rollup-plugin-esbuild'
 
-export default [
-    defineConfig({
-        input: './main.ts',
-        output: {
-            dir: './dist',
-        },
-        plugins: [
-            typescriptPaths({
-                preserveExtensions: true,
-                // absolute: false,
-            }),
-            esbuild(),
-        ],
-    }),
-]
+export default {
+    input: './main.ts',
+    output: {
+        dir: './dist',
+    },
+    plugins: [
+        typescriptPaths({
+            preserveExtensions: true,
+            // absolute: false,
+        }),
+        esbuild(),
+    ],
+}
